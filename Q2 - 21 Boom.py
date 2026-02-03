63e940837d6c3f38afd6d619330bab8d2640de11
GreatWolf155 <ygreenfield484@gmail.com>
2026-02-04 01:08:59 +0200

    Q1 and Q2 Completed

--- Q2 - 21 Boom.py
+++ Q2 - 21 Boom.py
@@ -45,8 +45,71 @@ If both totals are equal → draw
 
 import random
 
-# choose 10 random cards
-for _ in range(10):
-    suit = random.choice(["❤️", "♦️", "♣️", "♠️"])
-    card = random.choice([2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'])
-    print("Your card is:", card, suit)
+#initialize score
+player_1_power = 0
+player_2_power = 0
+deal = 1
+first_deal = 2
+
+while player_1_power <= 21 and deal == 1:
+    for i in range(first_deal):
+        player_suit = random.choice(["❤️", "♦️", "♣️", "♠️"])
+        player_card = random.choice([2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'])
+        print("Player 1's card is:", player_card, player_suit)
+        if player_card in ['J', 'Q', 'K']:
+            card_value = 10
+        elif player_card == 'A':
+            card_value = 1
+        else:
+            card_value = player_card
+        player_1_power += card_value
+
+    first_deal = 1
+    print("Player 1 current tally:", player_1_power)
+    if player_1_power >= 21:
+        break
+    print("Would you like to continue?")
+    print("CONTINUE (1)")
+    print("STOP (0)")
+    deal = int(input())
+if player_1_power == 21:
+    print("Player 1 wins!")
+elif player_1_power > 21:
+    print("Player 1 disqualified!")
+else:
+    #reinitialize
+    deal = 1
+    first_deal = 2
+
+    while player_2_power <= 21 and deal == 1:
+        for i in range(first_deal):
+            player_suit = random.choice(["❤️", "♦️", "♣️", "♠️"])
+            player_card = random.choice([2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'])
+            print("Player 2's card is:", player_card, player_suit)
+            if player_card in ['J', 'Q', 'K']:
+                card_value = 10
+            elif player_card == 'A':
+                card_value = 1
+            else:
+                card_value = player_card
+            player_2_power += card_value
+
+        first_deal = 1
+        print("Player 2 current tally:", player_2_power)
+        if player_2_power >= 21:
+            break
+        print("Would you like to continue?")
+        print("CONTINUE (1)")
+        print("STOP (0)")
+        deal = int(input())
+
+    if player_2_power > 21:
+        print("Player 2 disqualified!")
+    elif player_2_power == 21 or player_2_power > player_1_power:
+        print("Player 2 wins!")
+if player_2_power < player_1_power < 21:
+    print("Player 1 wins!")
+elif player_1_power == player_2_power:
+    print("Draw!")
+
+#note: 1am, im not dealing with duplicated cards right now
